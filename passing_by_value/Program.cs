fb2b36a5a9e7ceba1869a321acc88141e869b6de
Arsenie Sarmiento <111683775+arsenie-sarmiento@users.noreply.github.com>
2024-10-04 23:49:13 +1300

    Update Program.cs

--- passing_by_value/Program.cs
+++ passing_by_value/Program.cs
@@ -6,23 +6,23 @@ using System.Collections.Generic;
 
 class EmptyCell
 {
-    public string? willIChange;
+    public string? cellCharacter;
 }
 
 struct Barricade
 {
-    public string willIChange;
+    public string cellCharacter;
 }
 class TestClassAndStruct
 {
     static void ClassTaker(EmptyCell c)
     {
-        c.willIChange = "Changed";
+        c.cellCharacter = ".";
     }
 
     static void StructTaker(Barricade s)
     {
-        s.willIChange = "Changed";
+        s.cellCharacter = "X";
     }
 
     public static void Main()
@@ -30,19 +30,56 @@ class TestClassAndStruct
         EmptyCell testClass = new();
         Barricade testStruct = new();
 
-        testClass.willIChange = "Not Changed";
-        testStruct.willIChange = "Not Changed";
+        // Default: Not Changed
+        testClass.cellCharacter = " ";
+        testStruct.cellCharacter = " ";
 
         ClassTaker(testClass);
         StructTaker(testStruct);
 
-        Console.WriteLine("Class field = {0}", testClass.willIChange);
-        Console.WriteLine("Struct field = {0}", testStruct.willIChange);
+        Console.WriteLine("Class field = {0}", testClass.cellCharacter);
+        Console.WriteLine("Struct field = {0}", testStruct.cellCharacter);
+        PrintBoard();
         Console.ReadKey();
+    }
+
+    private static void PrintBoard()
+    {
+        //EmptyCell testClass = new();
+        Barricade testStruct = new();
+        int boardSize = 3;
+        for (int a = 1; a <= boardSize; a++)
+        {
+            char letter = (char)('A' + a - 1);
+            Console.Write($"    {letter} ");
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i <= boardSize; i++)
+        {
+            for (int h = 1; h <= boardSize; h++)
+            {
+                // Print inner grid: + ---
+                Console.Write(" + ---");
+            }
+
+            Console.Write("+");
+
+
+            Console.WriteLine();
+
+            for (int j = 1; j <= boardSize; j++)
+            {
+                Console.Write($" |  {testStruct.cellCharacter}  ");
+
+            }
+            // Print the vertical row counter (DOWN / RIGHT SIDE)
+            Console.Write($"| {i} ");
+            Console.WriteLine();
+        }
     }
 }
 /* Output:
     Class field = Changed
     Struct field = Not Changed
 */
-
